f7e46283f39e3e8dbc0c1fe2247ad7dd322b5008
Camalta <camaltra38@gmail.com>
2021-12-21 14:09:51 +0000

    Change statement

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *current;
 
-	if (head == NULL && *head == NULL)
+	if (*head == NULL && head == NULL)
 		return (NULL);
 	if (number < (*head)->n)
 	{
