b4da90795ddd7d12593d557342826ba3894a720a
Camaltra <mickael.boillaud@gmail.com>
2021-12-23 11:10:01 +0000

    Change the position of imported module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == '__main__':
     """Make some calcule :
             syntaxe : Usage: ./100-my_calculator.py <a> <operator> <b>"""
-
+    from calculator_1 import add, sub, mul, div
+    import sys
     numOfArg = len(sys.argv)
     if numOfArg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
