0dbc013a0e31b92530f6afb6150cabefee74284b
Camalta <camaltra38@gmail.com>
2021-12-23 10:13:00 +0000

    Adding the project

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == '__main__':
+    """Make some calcule :
+            syntaxe : Usage: ./100-my_calculator.py <a> <operator> <b>"""
+
+    numOfArg = len(sys.argv)
+    if numOfArg != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] == '-':
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == '*':
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        else:
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
