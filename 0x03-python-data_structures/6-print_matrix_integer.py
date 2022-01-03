7da6d676f8ec20c1f578b8d8ace5a67d2a895e90
Camaltra <mickael.boillaud@gmail.com>
2022-01-03 11:34:08 +0000

    Adding all python

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    if len(matrix) - 1 == 0:
+        print()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == len(matrix[i]) - 1:
+                print(matrix[i][j])
+            else:
+                print(matrix[i][j], end=" ")
