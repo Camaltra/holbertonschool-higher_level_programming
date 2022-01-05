240dd432710db7fbaec17a0c047b3b3ebdcfbc64
Camaltra <mickael.boillaud@gmail.com>
2022-01-05 11:01:35 +0000

    Maybe change synthaxe will work

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,8 @@ def weight_average(my_list=[]):
     totalScore = 0
     totalWeight = 0
     for elem in my_list:
-        score, weight = elem
+        score = elem[0]
+        weight = elem[1]
         totalScore += score * weight
         totalWeight += weight
     return totalScore / totalWeight
