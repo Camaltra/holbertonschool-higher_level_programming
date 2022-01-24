1217c23be7f777949f80f1403ad3db394b0cee90
Camaltra <mickael.boillaud@gmail.com>
2022-01-24 16:17:31 +0000

    Adding all task

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,45 @@
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not sys.argv[1].isdigit:
+        print("N must be a number")
+        exit(1)
+    numOfQueen = int(sys.argv[1])
+    if numOfQueen < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    res = []
+    for i in range(numOfQueen):
+        res.append([i, None])
+
+    def clearRes(x):
+        for i in range(x, numOfQueen):
+            res[i][1] = None
+
+    def reject(x, y):
+        for z in range(numOfQueen):
+            if y == res[z][1]:
+                return False
+        i = 0
+        while(i < x):
+            if abs(res[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def nqueens(x):
+        for y in range(numOfQueen):
+            clearRes(x)
+            if reject(x, y):
+                res[x][1] = y
+                if (x == numOfQueen - 1):
+                    print(res)
+                else:
+                    nqueens(x + 1)
+
+    nqueens(0)
