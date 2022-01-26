66ec9238dfb24e4c49c31fa07db1aeed0f74cb14
Camaltra <mickael.boillaud@gmail.com>
2022-01-26 08:48:52 +0000

    Adding length to all string object

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -16,9 +16,7 @@ void print_python_string(PyObject *p)
     if (PyUnicode_IS_COMPACT_ASCII(p))
         printf("  type: compact ascii\n");
     else
-    {
         printf("  type: compact unicode object\n");
-        printf("  length: %ld\n", ((PyASCIIObject *)(p))->length);
-    }
+    printf("  length: %ld\n", ((PyASCIIObject *)(p))->length)
     printf("  value: %ls\n", PyUnicode_AsWideCharString(p, NULL));
 }
