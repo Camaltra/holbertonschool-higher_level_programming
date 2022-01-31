b07b4de15c3fed56f001f5fcfad9d84cb64f320c
Camaltra <mickael.boillaud@gmail.com>
2022-01-31 17:03:16 +0000

    Modify the fomat of a print

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,10 +16,21 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """
+        Check if an given arg is correct
+        Args:
+            self (class)
+            name (str): The name of the var
+            value (int): The value to fill
+        Raises:
+            TypeError: If the value is not an int
+            ValueError: If the calue is less than of equal to 0
+        Returns: Anything
+        """
         if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
+            raise TypeError('{:s} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError('{:s} must be greater than 0'.format(name))
 
 
 class Rectangle(BaseGeometry):
