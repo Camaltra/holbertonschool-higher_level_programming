dcb7b22203ac838af828db59759a95a7e0a25f7c
Camaltra <mickael.boillaud@gmail.com>
2022-02-01 16:17:16 +0000

    Try something

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,7 +6,8 @@ Take stdin and check the input, make some operation on it
 
 import sys
 
-errorCode = {
+file_size = 0
+status_tally = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -16,29 +17,35 @@ errorCode = {
     "405": 0,
     "500": 0,
 }
-numOfLine = 0
-sumSize = 0
-
+i = 0
 try:
     for line in sys.stdin:
-        lineToken = line.split()
-        if lineToken[-2] in errorCode:
-            errorCode[lineToken[-2]] += 1
-        sumSize += int(lineToken[-1])
-        numOfLine += 1
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
 
-        if numOfLine % 10 == 0:
-            print("File size: {:d}".format(sumSize))
-            for key, value in sorted(errorCode.items()):
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(sumSize))
-    for key, value in sorted(errorCode.items()):
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(sumSize))
-    for key, value in sorted(errorCode.items()):
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
