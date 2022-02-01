587e28f95583c1e0fcd6d959e890cde3f7a05670
Camaltra <mickael.boillaud@gmail.com>
2022-02-01 14:50:23 +0000

    Adding all task

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Transform a class into a json object
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Transform a class into a json object
+    """
+    return json.dumps(vars(obj))
