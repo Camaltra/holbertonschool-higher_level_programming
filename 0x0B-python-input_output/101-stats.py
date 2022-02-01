194517f74268108f821c905a2925f9a77af2fbca
Camaltra <mickael.boillaud@gmail.com>
2022-02-01 16:02:44 +0000

    Try something

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -22,10 +22,17 @@ sumSize = 0
 try:
     for line in sys.stdin:
         lineToken = line.split()
+        tmp = numOfLine
         if lineToken[-2] in errorCode:
             errorCode[lineToken[-2]] += 1
-        sumSize += int(lineToken[-1])
-        numOfLine += 1
+            numOfLine += 1
+        try:
+            sumSize += int(lineToken[-1])
+            if tmp == numOfLine:
+                numOfLine += 1
+        except:
+            if tmp == numOfLine:
+                numOfLine += 1
 
         if numOfLine % 10 == 0:
             print("File size: {:d}".format(sumSize))
