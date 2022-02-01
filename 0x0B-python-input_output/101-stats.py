5d3f26b17027621fb507ed7c2e37622eaeb51907
Camaltra <mickael.boillaud@gmail.com>
2022-02-01 16:19:42 +0000

    Try something

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
 """
 Take stdin and check the input, make some operation on it
 """
-
 import sys
 
-file_size = 0
-status_tally = {
+errorCode = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -17,35 +14,37 @@ status_tally = {
     "405": 0,
     "500": 0,
 }
-i = 0
+numOfLine = 0
+sumSize = 0
+
 try:
     for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
+        lineToken = line.split()
+        if len(lineToken) >= 2:
+            tmp = numOfLine
+            if lineToken[-2] in errorCode:
+                errorCode[lineToken[-2]] += 1
+                numOfLine += 1
             try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
+                sumSize += int(lineToken[-1])
+                if tmp == numOfLine:
+                    numOfLine += 1
             except:
-                if a == i:
+                if tmp == numOfLine:
                     continue
 
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
+        if numOfLine % 10 == 0:
+            print("File size: {:d}".format(sumSize))
+            for key, value in sorted(errorCode.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
