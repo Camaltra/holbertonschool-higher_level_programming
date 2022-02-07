2992692e0cedcfc2ff55a54e1e8b40da788afd70
Camaltra <mickael.boillaud@gmail.com>
2022-02-07 12:36:20 +0000

    Change something

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,7 +30,7 @@ class Square(Rectangle):
         Returns: The infos about it
         """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.size
+            self.id, self.x, self.y, self.width
         )
 
     @property
@@ -39,7 +39,7 @@ class Square(Rectangle):
         Getter function for size
         Returns: The size var
         """
-        return self.__size
+        return self.witdh
 
     @size.setter
     def size(self, value):
@@ -51,7 +51,6 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
-        self.__size = value
 
     def update(self, *args, **kwargs):
         """
