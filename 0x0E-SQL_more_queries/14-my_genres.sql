5cd65a152cf8a425ddecd1ede4d3272822f17ab8
Camaltra <mickael.boillaud@gmail.com>
2022-03-09 15:06:13 +0000

    Change the order way

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 
 JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
 WHERE `tv_shows`.`title` = "Dexter"
-ORDER BY `tv_shows`.`title` DESC;
+ORDER BY `tv_genres`.`name`;
