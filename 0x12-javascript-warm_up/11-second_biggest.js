1e44d438e07dedf1d80e4f02b400b45616a6ffba
Camaltra <mickael.boillaud@gmail.com>
2022-04-04 14:24:17 +0000

    Style fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 
 const arrayArg = process.argv.slice(2);
 
-let secondMax = 0
+let secondMax = 0;
 if (arrayArg.length > 1) {
-  secondMax = arrayArg.sort(function(a, b) { return b - a; })[1]
+  secondMax = arrayArg.sort(function (a, b) { return b - a; })[1];
 }
 console.log(secondMax);
