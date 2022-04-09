d724928adab73e68c57a7277f063279f6f826f0b
Camaltra <mickael.boillaud@gmail.com>
2022-04-09 16:51:08 +0000

    Change some parameters on id City class

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -16,10 +16,7 @@ class City(Base):
     """Class : Cities"""
 
     __tablename__ = "cities"
-    id = Column(
-        Integer, autoincrement=True, unique=True,
-        nullable=False, primary_key=True
-    )
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(
         Integer,
