8d15f26b1307408ebca3fa71503b5812b12c7153
Camaltra <mickael.boillaud@gmail.com>
2022-04-09 16:44:20 +0000

    Change f.... by format

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
             session.query(City).filter(state.id == City.state_id).
             order_by(City.id)
         ):
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
     session.close()
