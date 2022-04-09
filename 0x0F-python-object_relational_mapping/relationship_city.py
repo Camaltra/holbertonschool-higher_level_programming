ceb8e69ef686761f098ffa00c8a7ddf3ebcf3f20
Camaltra <mickael.boillaud@gmail.com>
2022-04-09 17:28:07 +0000

    I'm bored

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,13 +4,10 @@
 Create a base of City, in relation with state
 """
 
-from sqlalchemy import ForeignKey
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class City(Base):
     """
@@ -21,7 +18,7 @@ class City(Base):
         name (int)
         state_id (int)
     """
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,10 +5,9 @@ Create a base State in relation with city
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from relationship_city import Base, City
-
 
 class State(Base):
     """Class : State
@@ -19,7 +18,7 @@ class State(Base):
         cities (list)
     """
 
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
+    cities = relationship("City", backref="state", cascade="all, delete")
