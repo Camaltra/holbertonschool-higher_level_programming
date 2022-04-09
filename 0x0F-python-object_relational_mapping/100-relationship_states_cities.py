170f82ad074e00eb4baed8c4db55a6ba02f36a9c
Camaltra <mickael.boillaud@gmail.com>
2022-04-09 17:39:39 +0000

    Adding Author

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-
 """
-Create a cities, related to a state
-adding line of comment
+creates the State California with the City San Francisco from the database
 """
 
-import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
+from sys import argv
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(sys.argv[1],
-                                       sys.argv[2],
-                                       sys.argv[3]),
-                               pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    session.add_all([new, new_city])
-
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    cali = State(name="California")
+    cali.cities = [City(name="San Francisco")]
+    session.add(cali)
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-
 """
-Create a base of City, in relation with state
-adding line of comment
+contains the class City
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-
-Base = declarative_base()
+from relationship_state import Base, State
 
 
 class City(Base):
-    """Class City"""
-
-    __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+    """Representation of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
