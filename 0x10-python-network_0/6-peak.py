51b91d5b030d08d4fb9985109b1519d80a2c3352
Camaltra <mickael.boillaud@gmail.com>
2022-04-21 12:07:35 +0000

    Add docs and pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,28 @@ Interview questions peak
 
 
 def find_peak(list_of_integers):
+    """
+    Find a peak in a list of numbers
+    args:
+        list_of_intergers (list): List of int
+    Return: None of not peak, the peak else
+    """
     if len(list_of_integers) == 0:
         return None
-    if len (list_of_integers) == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
     return find_peak_rec(list_of_integers, 0, len(list_of_integers))
 
+
 def find_peak_rec(nums, start, end):
+    """
+    Find a peak in a list of numbers via recurtions
+    args:
+        num (list): List of int
+        start (int): The idx start
+        end (int): The idx end
+    Return: None of not peak, the peak else
+    """
     if start > end:
         return None
 
@@ -23,7 +38,7 @@ def find_peak_rec(nums, start, end):
         return nums[middle]
     if nums[middle - 1] < nums[middle] and nums[middle + 1] < nums[middle]:
         return nums[middle]
-    
+
     if nums[middle - 1] > nums[middle]:
         return find_peak_rec(nums, start, middle - 1)
     else:
