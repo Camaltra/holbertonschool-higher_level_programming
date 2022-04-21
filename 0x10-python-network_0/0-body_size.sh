558a329fe81603bf46eb8880cd2ceff211f233a4
Camaltra <mickael.boillaud@gmail.com>
2022-04-21 11:54:53 +0000

    Fix() 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Create a curl requests, and display the size in bytes of the response
-curl -Is "$1" | grep "Content-Length" | cut -d ' ' -f2
+curl -sI "$1" | grep 'Content-Length:' | cut -d ' ' -f2
