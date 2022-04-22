10bb5200488eaedef2009f3908e00a3693d02808
Camaltra <mickael.boillaud@gmail.com>
2022-04-22 08:46:29 +0000

    Fix() 100, mistake avout repo name and author

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
-                                                                sys.argv[1],
-                                                                sys.argv[2]))
+                                                                sys.argv[2],
+                                                                sys.argv[1]))
     count = 0
     for commit in r.json():
         if count > 9:
