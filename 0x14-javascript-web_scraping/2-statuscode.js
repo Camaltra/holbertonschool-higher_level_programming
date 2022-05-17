9a17965232e11478edbf400876ae09dee4ad1be8
Camaltra <mickael.boillaud@gmail.com>
2022-05-17 11:34:52 +0000

    roll back

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
- const axios = require('axios');
+const axios = require('axios');
 
- const URL = process.argv[2];
+const URL = process.argv[2];
 
- async function getMethod (URL) {
-   await axios.get(URL)
-     .then((res) => {
-       console.log(`code: ${res.status}`);
-     }).catch((err) => {
-       console.log(`code: ${err.response.status}`);
-     });
- }
+async function getMethod (URL) {
+  await axios.get(URL)
+    .then((res) => {
+      console.log(`code: ${res.status}`);
+    }).catch((err) => {
+      console.log(`code: ${err.response.status}`);
+    });
+}
 
- getMethod(URL);
+getMethod(URL);
