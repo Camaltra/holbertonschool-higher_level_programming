a95f4f7af8f5696afbb52ca60165886ef1241f9e
Camaltra <mickael.boillaud@gmail.com>
2022-05-17 13:19:03 +0000

    Fix() 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const pathFile = process.argv[2];
-
-fs.readFile(`./${pathFile}`, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    return console.log(err);
   }
+  console.log(data);
 });
